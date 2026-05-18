069d2f02f6aa15e5f52ef78e53e85561db9af7e1
Lars Ritland <lars.ritland@crayon.no>
2026-05-18 22:29:56 +0200

    chore: remove target/ and logs/ from tracking

--- tasty_bytes/target/compiled/tasty_bytes/models/marts/dim_location.sql
+++
@@ -1,9 +0,0 @@
-select
-    location_id,
-    placekey,
-    location_name,
-    city,
-    region,
-    iso_country_code,
-    country
-from LARSR_DB.DEV_LARSR_TASTY_BYTES.stg_locations
--- tasty_bytes/target/compiled/tasty_bytes/models/staging/stg_locations.sql
+++
@@ -1,9 +0,0 @@
-select
-    LOCATION_ID as location_id,
-    PLACEKEY as placekey,
-    LOCATION as location_name,
-    CITY as city,
-    REGION as region,
-    ISO_COUNTRY_CODE as iso_country_code,
-    COUNTRY as country
-from LARSR_DB.RAW.location
